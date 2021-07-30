2874a07ec2430f709bbcab277b50d85afc79f919
Dayo Opedare <djetlii@live.com>
2021-07-30 15:11:55 +0100

    sign in

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,23 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 
 
 
 
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -6,6 +6,8 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = "smtp-mail.outlook.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
